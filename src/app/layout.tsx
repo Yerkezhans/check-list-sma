75458a20ef5f652d1e4ff91c9fe0eb6fa3a37a4f
yerkezhan <serikkalieva_erkezhan@mail.ru>
2026-02-15 03:04:43 +0500

    Remove Google Font to fix docker build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,10 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Montserrat } from 'next/font/google';
-
-const montserrat = Montserrat({
-  subsets: ['latin'],
-  weight: ['400', '700'],
-  style: ['normal', 'italic'],
-  display: 'swap',
-  variable: '--font-montserrat',
-});
-
 
 export const metadata: Metadata = {
   title: "Калькулятор риска СМА",
   description: "Калькулятор риска СМА",
-  keywords: "Калькулятор риска СМА"
+  keywords: "Калькулятор риска СМА",
 };
 
 export default function RootLayout({
@@ -25,10 +15,8 @@ export default function RootLayout({
   params: { locale: string };
 }) {
   return (
-    <html lang={params.locale} className={`${montserrat.variable}`}>
-      <body>
-        {children}
-      </body>
+    <html lang={params.locale}>
+      <body>{children}</body>
     </html>
   );
 }
